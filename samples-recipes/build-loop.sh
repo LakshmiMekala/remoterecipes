ce2deb16f78c96d0e49fa1cc55a5e00477607d56
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-12-13 11:56:06 +0530

    check 1

--- samples-recipes/build-loop.sh
+++ samples-recipes/build-loop.sh
@@ -203,6 +203,7 @@ function recipe_registry()
                             eval xpath_recipe='.recipe_repos[$j].publish[$x].recipe' ;
                             Gateway[$x]=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_recipe) ;
                             Gateway[$x]=$(echo ${Gateway[$x]} | tr -d '"') ;
+                            echo %%%%%%%%%%"${Gateway[$x]}"%%%%%%%%%%%%%
                         done
                         echo "${Gateway[@]}" ;
                         RecipesToBeDeleted ;
@@ -217,7 +218,7 @@ function recipe_registry()
                     Gateway[$x]=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_recipe) ;
                     Gateway[$x]=$(echo ${Gateway[$x]} | tr -d '"') ;
                     #recipeCreate[$y]=${Gateway[$x]} ;
-                    recipeInfo ;
+                    #recipeInfo ;
                     if [[ $OPTIMIZE = TRUE ]] ; then
                         if [[ $recipeName =~ ${Gateway[$x]}/${Gateway[$x]}.json ]] || [[ $recipeName =~ ${Gateway[$x]}/manifest ]];then
                             # echo "${Gateway[$x]} found in current commit" ;
@@ -236,7 +237,7 @@ function recipe_registry()
                     fi                    
                 done
                 echo "list of gws available in registry is ${Gateway[@]}";
-                RecipesNewlyAdded ;
+                #RecipesNewlyAdded ;
             done
             #RecipesToBeCreated ;
             #echo ${recipearray[@]};
