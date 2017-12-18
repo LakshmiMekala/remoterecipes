fa1e5641b5f3893eb426379ca7b7b7872566bbbb
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-12-18 11:36:02 +0530

    creating branch for gotests

---
+++ go-test/report.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name="${TRAVIS_REPO_SLUG}" ;
+namefolder=${name:14} ;
+
+destFolder="$namefolder-${TRAVIS_TAG}"
+
+pushd $GOPATH/src/github.com/TIBCOSoftware/mashling ;
+
+mkdir -p "$destFolder";
+cd "$destFolder";
+cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/go-test-result.html" "$GOPATH/src/github.com/TIBCOSoftware/mashling/$destFolder/go-test-result.html" ;
+popd;
