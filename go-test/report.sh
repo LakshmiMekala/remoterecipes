b85e6938633c8709b7841340fa33d8437cd90f05
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-12-18 15:42:59 +0530

    commiting final report build file

--- go-test/report.sh
+++ go-test/report.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-name="${TRAVIS_REPO_SLUG}" ;
-namefolder=${name:14} ;
-echo "$namefolder";
-
 pushd $GOPATH/src/github.com/TIBCOSoftware/mashling ;
 go-test-html gotest_stdout_file gotest_stderr_file go-test-result.html
-    if [ -n "${TRAVIS_TAG}" ]; then
-        destFolder="$namefolder-${TRAVIS_TAG}"
-    elif [ -z "${TRAVIS_TAG}" ]; then
-        destFolder="$namefolder-${TRAVIS_BUILD_NUMBER}"
-    fi
-
-    if [ ! -d "$destFolder" ]; then
-        mkdir "$destFolder";
-    fi
-    echo "Creating folder - $destFolder /"
-    cd "$destFolder";
-
-cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/go-test-result.html" "$GOPATH/src/github.com/TIBCOSoftware/mashling/$destFolder/go-test-result.html"
+mkdir reports;
+cd reports;
+cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/go-test-result.html" "$GOPATH/src/github.com/TIBCOSoftware/mashling/reports/go-test-result.html"
 cd ..;
 rm -rf Godeps
 rm gotest_stdout_file gotest_stderr_file go-test-result.html
 popd ;
-
