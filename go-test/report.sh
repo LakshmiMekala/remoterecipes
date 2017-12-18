3b0216df5526f9cfa3214b9fd7b2205cbd55f087
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-12-18 13:53:16 +0530

    creating only report.html file

--- go-test/report.sh
+++ go-test/report.sh
@@ -5,34 +5,20 @@ namefolder=${name:14} ;
 echo "$namefolder";
 
 pushd $GOPATH/src/github.com/TIBCOSoftware/mashling ;
+    if [ -n "${TRAVIS_TAG}" ]; then
+        destFolder="$namefolder-${TRAVIS_TAG}"
+    elif [ -z "${TRAVIS_TAG}" ]; then
+        destFolder="$namefolder-${TRAVIS_BUILD_NUMBER}"
+    fi
 
-cat > abc <<EOL
-sample textfile
-EOL
-cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/abc" "$HOME/abc" ;
+    if [ ! -d "$destFolder" ]; then
+        mkdir "$destFolder";
+    fi
+    echo "Creating folder - $destFolder /"
+    cd "$destFolder";
+
+cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/go-test-result.html" "$GOPATH/src/github.com/TIBCOSoftware/mashling/$destFolder/go-test-result.html"
+rm -rf Godeps
+rm gotest_stdout_file gotest_stderr_file go-test-result.html
 popd ;
 
-
-# pushd $HOME
-#     if [ -n "${TRAVIS_TAG}" ]; then
-#         destFolder="$namefolder-${TRAVIS_TAG}"
-#     elif [ -z "${TRAVIS_TAG}" ]; then
-#         destFolder="$namefolder-${TRAVIS_BUILD_NUMBER}"
-#     fi
-
-#     if [ ! -d "$destFolder" ]; then
-#         mkdir "$destFolder";
-#     fi
-#     echo "Creating folder - $destFolder /"
-#     cd "$destFolder";
-
-# #pushd $GOPATH/src/github.com/TIBCOSoftware/mashling ;
-
-# #mkdir -p "$destFolder";
-# #cd "$destFolder";
-# #cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/go-test-result.html" "$GOPATH/src/github.com/TIBCOSoftware/mashling/$destFolder/go-test-result.html" ;
-# cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/go-test-result.html" "$HOME/$destFolder" ;
-# echo "files in directory";
-# ls;
-# cp 
-# popd;
