ad59bc78e4bee8414339aa8a699cf965e56b03fb
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-01-02 12:55:37 +0530

    upload

--- go-test/report-copy.sh
+++ go-test/report-copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-name="${TRAVIS_REPO_SLUG}" ;
-namefolder=${name:14} ;
+REPONAME="${TRAVIS_REPO_SLUG}" ;
+REPOFOLDER=${REPONAME:14} ;
 
 mkdir ${HOME}/.aws
 cat > ${HOME}/.aws/credentials <<EOL
@@ -10,35 +10,34 @@ aws_access_key_id = ${SITE_KEY}
 aws_secret_access_key = ${SITE_KEY_SECRET}
 EOL
 
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then    
+    echo "cleaning S3 folder"
+    aws s3 rm s3://$AWS_BUCKET/LakshmiMekala/remoterecipes/go-tests/latest --recursive
+fi
+
 function create_dest_directory ()
 {
     cd reports ;
     if [ -n "${TRAVIS_TAG}" ]; then
-        destFolder="$namefolder-${TRAVIS_TAG}"
+        DESTFOLDER="$REPOFOLDER-${TRAVIS_TAG}"
     elif [ -z "${TRAVIS_TAG}" ]; then
-        destFolder="$namefolder-${TRAVIS_BUILD_NUMBER}"
+        DESTFOLDER="$REPOFOLDER-${TRAVIS_BUILD_NUMBER}"
     fi
 
-    if [ ! -d "$destFolder" ]; then
-        mkdir "$destFolder" "latest";
+    if [ ! -d "$DESTFOLDER" ]; then
+        mkdir "$DESTFOLDER" "latest";
     fi
-    echo "Creating folder - $destFolder /"
-    cd "$destFolder";
+    echo "Creating folder - $DESTFOLDER /"
+    cd "$DESTFOLDER";
 }
 
 pushd $GOPATH/src/github.com/TIBCOSoftware/mashling ;
-go-test-html gotest_stdout_file gotest_stderr_file go-test-result.html
-
+go-test-html gotest_stdout_file gotest_stderr_file go-test-result.html;
 mkdir -p reports;
 create_dest_directory ;
-
-ls;
-
-cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/go-test-result.html" "$GOPATH/src/github.com/TIBCOSoftware/mashling/reports/$destFolder"
+cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/go-test-result.html" "$GOPATH/src/github.com/TIBCOSoftware/mashling/reports/$DESTFOLDER"
 cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/go-test-result.html" "$GOPATH/src/github.com/TIBCOSoftware/mashling/reports/latest"
-
 aws s3 cp "$GOPATH/src/github.com/TIBCOSoftware/mashling/reports" "s3://$AWS_BUCKET/LakshmiMekala/remoterecipes/go-tests" --recursive
-
 popd ;
 
 
