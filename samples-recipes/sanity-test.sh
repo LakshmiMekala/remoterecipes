8f9e9e141299bfe603ed8963aa10d475c83a1d09
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-01-17 16:58:57 +0530

    v1

--- samples-recipes/sanity-test.sh
+++ samples-recipes/sanity-test.sh
@@ -2,10 +2,8 @@
 
 function sanity-test()
 {
-    cat "$GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/${remotereponame[$j]}/${recipeCreate[$x]}/${recipeCreate[$x]}.sh"
     if [[ -f "$GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/${remotereponame[$j]}/${recipeCreate[$x]}/${recipeCreate[$x]}.sh" ]];then        
         pushd "$GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/${remotereponame[$j]}/${recipeCreate[$x]}";
-        ls ;
         source ./${recipeCreate[$x]}.sh
         value=($(get_test_cases))
         sleep 1
@@ -30,10 +28,8 @@ function sanity-test()
                 sed -i "s/<\/tr> <\/table>/<tr><td>${provider[$j]}<\/td><td>${recipeCreate[$x]}<\/td><td>${value[i]}<\/td><td  class="error">FAIL<\/td><\/tr><\/tr> <\/table>/g" $GOPATH/$FILENAME
             fi
             p=$((p+1));
-            echo $p;
         done
         popd
-    #    p=$((p+1))
     else
         echo "Sanity file does not exist"
         sed -i "s/<\/tr> <\/table>/<tr><td>${provider[$j]}<\/td><td>${recipeCreate[$x]}<\/td><td>NA<\/td><td>NA<\/td><\/tr><\/tr> <\/table>/g" $GOPATH/$FILENAME
@@ -53,7 +49,7 @@ function recipesToBeTested()
 cd $GOPATH
 FILENAME="SanityReport.html"
 HTML="<!DOCTYPE html>
-<html><head><style>table {font-family: arial, sans-serif;border-collapse: collapse;margin: auto;}td,th {border: 1px solid #dddddd;text-align: left;padding: 8px;}th {background: #003399;text-align: center;color: #fff;}body {padding-right: 15px;padding-left: 15px;margin-right: auto;margin-left: auto;}label {font-weight: bold;}.test-report h1 {color: #003399;}.summary,.test-report {text-align: center;}.success {background-color: #79d279;}.error {background-color: #ff3300;}.summary-tbl {font-weight: bold;}.summary-tbl td {border: none;}</style></head><body>    <section class=test-report><h1>Recipes Sanity report</h1></section><section class=summary><h2>Summary</h2><table class="summary-tbl"><tr><td>Number of test cases passed </td> <td> </td></tr><tr><td>Number of test cases failed </td> <td> </td></tr><td>Total test cases</td><td></td></tr></tr></table></section><section class=test-report><h2>Detailed report</h2><table><tr><th>Provider</th><th>Recipe</th><th> Testcase </th><th>Status</th><tr></tr> </table></html>"
+<html><head><style>table {font-family: arial, sans-serif;border-collapse: collapse;margin: auto;}td,th {border: 1px solid #dddddd;text-align: left;padding: 8px;}th {background: #003399;text-align: center;color: #fff;}body {padding-right: 15px;padding-left: 15px;margin-right: auto;margin-left: auto;}label {font-weight: bold;}.test-report h1 {color: #003399;}.summary,.test-report {text-align: center;}.success {background-color: #79d279;}.error {background-color: #ff3300;}.summary-tbl {font-weight: bold;}.summary-tbl td {border: none;}</style></head><body>    <section class=test-report><h1>Recipes Sanity Report</h1></section><section class=summary><h2>Summary</h2><table class="summary-tbl"><tr><td>Number of test cases passed </td> <td> </td></tr><tr><td>Number of test cases failed </td> <td> </td></tr><td>Total test cases</td><td></td></tr></tr></table></section><section class=test-report><h2>Detailed report</h2><table><tr><th>Provider</th><th>Recipe</th><th> Testcase </th><th>Status</th><tr></tr> </table></html>"
 
 echo $HTML >> $FILENAME
 
@@ -67,7 +63,6 @@ fi
 
 array_length=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq '.recipe_repos | length') ;
 echo "Found $array_length recipe providers." ;
-count=();
     p=0;
     q=0;
     r=0;
@@ -97,9 +92,7 @@ for (( j = 0; j < $array_length; j++ ))
         fi
         recipeCreated=$(cat $GOPATH/recipes-[$j]);
         recipesToBeTested;
-        echo "iiiiiiiiiiiiiiiiiiiii"
         echo gateway array is "${recipeCreate[@]}";
-        echo "jjjjjjjjjjjjjjjjjjjjjjjj";
         for (( x=0; x<"${#recipeCreate[@]}"; x++ ))
         do
             sanity-test;
