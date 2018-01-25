e16642574389745f8ff6099a06a347dd025307ac
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-01-25 11:05:28 +0530

    version 2

--- samples-recipes/final-remoteurl.sh
+++ samples-recipes/final-remoteurl.sh
@@ -203,9 +203,10 @@ function recipe_registry()
 				eval xpath_recipe='.recipe_repos[$j].publish[$x].recipe' ;
 				Gateway[$x]=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/recipe_registry.json | jq $xpath_recipe) ;
 				Gateway[$x]=$(echo ${Gateway[$x]} | tr -d '"') ;
-                 displayImage=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/"${remotereponame[$j]}"/"${Gateway[$x]}"/"${Gateway[$x]}".json | jq '.gateway.display_image') ;
-                    displayImage=$(echo $displayImage | tr -d '"');
-                    echo "=====================$displayImage========================="
+                unset displayImage
+                displayImage=$(cat $GOPATH/src/github.com/TIBCOSoftware/mashling-recipes/"${remotereponame[$j]}"/"${Gateway[$x]}"/"${Gateway[$x]}".json | jq '.gateway.display_image') ;
+                displayImage=$(echo $displayImage | tr -d '"');
+                echo "=====================$displayImage========================="
 				recipeInfo ;
                 z=$z+1
 				if [[ $OPTIMIZE = TRUE ]] ; then
