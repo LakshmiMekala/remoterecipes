7215d657c33a7acaf9046af8e69af4d4386a1759
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-02-02 12:05:49 +0530

    checking local+travis v2

--- samples-recipes/source.sh
+++ samples-recipes/source.sh
@@ -2,6 +2,6 @@
 
 #Create recipes and run sanity tests
 
-    chmod ugo+x ./local.sh ./local-sanity.sh
+    chmod ugo+x ./local-recipes.sh ./local-sanity.sh
     ./local-recipes.sh
     ./local-sanity.sh
